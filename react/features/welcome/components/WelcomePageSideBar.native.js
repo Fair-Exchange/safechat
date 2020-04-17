aa265d1062d8670eda3e2afd311ef919bcfabcb7
Sol Ham <solham@Sols-MacBook-Air.local>
2020-04-17 14:53:38 -0600

    privacy and terms html

--- react/features/welcome/components/WelcomePageSideBar.native.js
+++ react/features/welcome/components/WelcomePageSideBar.native.js
@@ -25,12 +25,12 @@ import styles, { SIDEBAR_AVATAR_SIZE } from './styles';
 /**
  * The URL at which the privacy policy is available to the user.
  */
-const PRIVACY_URL = 'https://safe.chat/privacy';
+const PRIVACY_URL = 'https://safe.chat/privacy.html';
 
 /**
  * The URL at which the terms (of service/use) are available to the user.
  */
-const TERMS_URL = 'https://safe.chat/terms';
+const TERMS_URL = 'https://safe.chat/terms.html';
 
 type Props = {
 
